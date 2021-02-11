2e688221f792affd7f1b30a5b5ab36ca69f173df
James VanderKam <jamesvanderkam@verizon.net>
2021-02-11 14:10:03 -0500

    basic plan created

---
+++ script.js
@@ -0,0 +1,10 @@
+//When the page loads, generate all the blocks
+    //filled from local storage
+    //add css class based on past, present, future
+    //add to container
+
+//insert clock into the today's date
+    //day of week, month, date
+
+//enter text into a time block, saved in local storage
+    //clickable button to save it
