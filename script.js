d9d1d1ebdaf68afa0c75db2acc73d46c23954ed3
James VanderKam <jamesvanderkam@verizon.net>
2021-02-13 17:23:43 -0500

    time blocks appear

--- script.js
+++ script.js
@@ -16,30 +16,42 @@
     //add to container
 var loadSchedule = function(){
     var timesMorning = ["09","10","11","12","01","02","03","04","05"];
-    var past = true;
-    var schedule = $('#container')
+    var past = true; //only if day already started, fix
+    var schedule = $('#timeblock');
     for (var i=0;i<timesMorning.length;i++){
         var number = moment().format("hh");
         console.log(".."+number+".."+timesMorning[i]);
 
         var scheduleSlot = $('<div>');
         scheduleSlot.addClass("time-block");
+        scheduleSlot.addClass("row");
 
-        var timeSlot = $('<p>');
-        timeSlot.addClass("hour").text(timesMorning[i]);
+        var timeSlot = $('<p>'+timesMorning[i]+'</p>');
+        timeSlot.addClass("hour");
 
+        var scheduleText = $('<p>');
+        var text = " PLACEHOLDER ";
         if(timesMorning[i]==number){
+            console.log("present");
             past = false;
-            scheduleSlot.addClass('present');
+            scheduleText.addClass('present').text(text);
         }
         else if(past){
-            scheduleSlot.addClass('past');
+            console.log("past");
+            scheduleText.addClass('past').text(text);
             //it's past
         }
         else{
-            scheduleSlot.addClass('future');
+            console.log("future");
+            scheduleText.addClass('future').text(text);
             //future
         }
+        var saveButton = $('<p>');
+        saveButton.addClass('saveBtn').text("X");
+        scheduleSlot.append(timeSlot);
+        scheduleSlot.append(scheduleText);
+        scheduleSlot.append(saveButton);
+        schedule.append(scheduleSlot);
 
 
     }
